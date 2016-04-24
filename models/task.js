c64f63a1de51eb4dc558a7d868b1372a83caa494
Diego Camargo <dicamargov@gmail.com>
2016-04-24 17:31:29 -0500

    changed to 2 spaces indentation

--- models/task.js
+++ models/task.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/tasks');
@@ -7,15 +7,15 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 var TaskSchema = new mongoose.Schema({
-	_id: String,
-	title: {type: String, required: [true, 'No title? Really dude?']},
-	description: String,
-	dueDate: {type: Date, required: [true, 'If you don\'t know when this is due for use another app']},
-	reminder: {type: Boolean, default: false},
-	completed: {type: Boolean, default: false},
-	dateCreated: {type: Date, default: Date.now},
-	dateUpdated: {type: Date, default: Date.now},
-	reminderDate: Date
+  _id: String,
+  title: {type: String, required: [true, 'No title? Really dude?']},
+  description: String,
+  dueDate: {type: Date, required: [true, 'If you don\'t know when this is due for use another app']},
+  reminder: {type: Boolean, default: false},
+  completed: {type: Boolean, default: false},
+  dateCreated: {type: Date, default: Date.now},
+  dateUpdated: {type: Date, default: Date.now},
+  reminderDate: Date
 });
 
 exports.Task = mongoose.model('Task', TaskSchema);
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var Task = require('../models/task').Task;
 var uuid = require('node-uuid');
@@ -7,42 +7,42 @@ exports.findById = function(req, res) {
     var id = req.params.id;
     console.log('Retrieving task: ' + id);
     Task.findById(id, function(err, task) {
-    	if (err) {
-    		res.status(500).send(err);
-    	} else {
-    		res.send(task);
-    	}
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(task);
+      }
     });
 };
 
 exports.findAll = function(req, res) {
     console.log('Retrieving all tasks');
-  	Task.find(function (err, tasks) {
-  		if (err) {
-  			console.log(err);
-  			res.status(500).send(err);
-  		} else {
-  			res.send(tasks);
-  		}
-  	});
+    Task.find(function (err, tasks) {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send(tasks);
+      }
+    });
 };
 
 exports.addTask = function(req, res) {
     var task = req.body;    
     console.log('Adding task: ' + JSON.stringify(task));
     if (task._id == undefined) {
-    	task._id = uuid.v4();
+      task._id = uuid.v4();
     }
     if (task.reminder && task.reminderDate == undefined) {
-    	task.reminderDate = Date.now + 86400000
+      task.reminderDate = Date.now + 86400000
     }
     Task.create(task, function(err, task) {
-    	if (err) {
-    		console.log(err);
-    		res.status(400).send(err);
-    	} else {
-    		res.status(201).send(task);
-    	}
+      if (err) {
+        console.log(err);
+        res.status(400).send(err);
+      } else {
+        res.status(201).send(task);
+      }
     });
     
 }
@@ -51,30 +51,30 @@ exports.updateTask = function(req, res) {
     var id = req.params.id;
     var updatedValues = req.body;
     Task.findById(id, function(err, task) {
-    	if (err) {
-    		console.log(err);
-    		res.status(500).send(err);
-    	} else {
-    		task.dateUpdated = Date.now();
-    		task.title = updatedValues.title != null ? updatedValues.title : task.title;
-    		task.reminder = updatedValues.reminder != null ? updatedValues.reminder : task.reminder;
-    		task.completed = updatedValues.completed != null ? updatedValues.completed : task.completed;
-    		task.reminderDate = updatedValues.reminderDate != null ? updatedValues.reminderDate : task.reminderDate;
-    		if (task.description != undefined) {
-    			task.description = updatedValues.description != null ? updatedValues.description : task.description;
-    		}
-    		if (task.reminderDate != undefined) {
-    			task.reminderDate = updatedValues.reminderDate != null ? updatedValues.reminderDate : task.description;
-    		}
-    		task.save(function(err1) {
-    			if (err1) {
-    				console.log(err1);
-    				res.status(500).send(err1);
-    			} else {
-    				res.send(task);
-    			}
-    		})
-    	}
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        task.dateUpdated = Date.now();
+        task.title = updatedValues.title != null ? updatedValues.title : task.title;
+        task.reminder = updatedValues.reminder != null ? updatedValues.reminder : task.reminder;
+        task.completed = updatedValues.completed != null ? updatedValues.completed : task.completed;
+        task.reminderDate = updatedValues.reminderDate != null ? updatedValues.reminderDate : task.reminderDate;
+        if (task.description != undefined) {
+          task.description = updatedValues.description != null ? updatedValues.description : task.description;
+        }
+        if (task.reminderDate != undefined) {
+          task.reminderDate = updatedValues.reminderDate != null ? updatedValues.reminderDate : task.description;
+        }
+        task.save(function(err1) {
+          if (err1) {
+            console.log(err1);
+            res.status(500).send(err1);
+          } else {
+            res.send(task);
+          }
+        })
+      }
     });
 
 }
@@ -83,11 +83,11 @@ exports.deleteTask = function(req, res) {
     var id = req.params.id;
     console.log('Deleting task: ' + id);
     Task.findById(id, function(err, task) {
-    	if (err) {
-    		res.status(500).send(err);
-    	} else {
-    		task.remove();
-    		res.send(task);
-    	}
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        task.remove();
+        res.send(task);
+      }
     });
 }
